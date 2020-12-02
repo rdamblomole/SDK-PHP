8bdd5f0168c982b47811046edad2585a0248456f
Ruben David <ramblomole@lyracons.com>
2020-12-02 14:13:48 -0300

    V1.10.15 Update composer.json - endpoint developer

--- TodoPago/lib/Sdk.php
+++ TodoPago/lib/Sdk.php
@@ -4,7 +4,7 @@ namespace TodoPago;
 require_once(dirname(__FILE__)."/Client.php");
 
 define('TODOPAGO_VERSION','1.10.2');
-define('TODOPAGO_ENDPOINT_TEST','https://developers.todopago.com.ar/');
+define('TODOPAGO_ENDPOINT_TEST','https://apis.developers.todopago.com.ar/');
 define('TODOPAGO_ENDPOINT_PROD','https://apis.todopago.com.ar/');
 define('TODOPAGO_ENDPOINT_TENATN', 't/1.1/');
 define('TODOPAGO_ENDPOINT_SOAP_APPEND', 'services/');
